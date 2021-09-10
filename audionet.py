7d0232c94ad331eb9a6525b1a2d5c0290260ae1e
karan-sarkar <karan@sarkarfamily.org>
2021-09-09 21:29:43 -0400

    Use 1d Conv network with lots of channels

--- audionet.py
+++ audionet.py
@@ -17,43 +17,29 @@ class AudioNet(nn.Module):
     def __init__(self, n_classes=1251):
         super(AudioNet, self).__init__()
         self.n_classes=n_classes
-        self.block1=nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(7,7), stride=(2,2), padding=1)),
-            ('bn1', nn.BatchNorm2d(96, momentum=0.5)),
-            ('relu1', nn.ReLU())]))
-        
-        self.extract1 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(1,1), stride=(1,1), padding=0)
-            
-        self.block2=nn.Sequential(OrderedDict([           
-            ('mpool1', nn.MaxPool2d(kernel_size=(3,3), stride=(2,2))),
-            ('conv2', nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(5,5), stride=(2,2), padding=1)),
-            ('bn2', nn.BatchNorm2d(256, momentum=0.5)),
-            ('relu2', nn.ReLU())]))
-            
-        self.extract2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(1,1), stride=(1,1), padding=0)
-          
-        self.block3=nn.Sequential(OrderedDict([              
-            ('mpool2', nn.MaxPool2d(kernel_size=(3,3), stride=(2,2))),
-            ('conv3', nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(3,3), stride=(1,1), padding=1)),
-            ('bn3', nn.BatchNorm2d(384, momentum=0.5)),
+        self.features=nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv1d(in_channels=512, out_channels=2048, kernel_size=(7), stride=(2), padding=1)),
+            ('bn1', nn.BatchNorm1d(2048, momentum=0.5)),
+            ('relu1', nn.ReLU()),
+            ('mpool1', nn.MaxPool1d(kernel_size=(3), stride=(2))),
+            ('conv2', nn.Conv1d(in_channels=2048, out_channels=2048, kernel_size=(5), stride=(2), padding=1)),
+            ('bn2', nn.BatchNorm1d(2048, momentum=0.5)),
+            ('relu2', nn.ReLU()),
+            ('mpool2', nn.MaxPool1d(kernel_size=(3), stride=(2))),
+            ('conv3', nn.Conv1d(in_channels=2048, out_channels=4096, kernel_size=(3), stride=(1), padding=1)),
+            ('bn3', nn.BatchNorm1d(4096, momentum=0.5)),
             ('relu3', nn.ReLU()),
-            ('conv4', nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=1)),
-            ('bn4', nn.BatchNorm2d(256, momentum=0.5)),
+            ('conv4', nn.Conv1d(in_channels=4096, out_channels=2048, kernel_size=(3), stride=(1), padding=1)),
+            ('bn4', nn.BatchNorm1d(2048, momentum=0.5)),
             ('relu4', nn.ReLU()),
-            ('conv5', nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=1)),
-            ('bn5', nn.BatchNorm2d(256, momentum=0.5)),
-            ('relu5', nn.ReLU())]))
-        
-        self.extract3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(1,1), stride=(1,1), padding=0)        
-         
-        self.block4=nn.Sequential(OrderedDict([         
-            ('mpool5', nn.MaxPool2d(kernel_size=(5,3), stride=(3,2))),
-            ('fc6', nn.Conv2d(in_channels=256, out_channels=4096, kernel_size=(9,1), stride=(1,1))),
-            ('bn6', nn.BatchNorm2d(4096, momentum=0.5)),
-            ('relu6', nn.ReLU())]))  
-         
-        self.block5=nn.Sequential(OrderedDict([       
-            ('apool6', nn.AdaptiveAvgPool2d((1,1))),
+            ('conv5', nn.Conv1d(in_channels=2048, out_channels=2048, kernel_size=(3), stride=(1), padding=1)),
+            ('bn5', nn.BatchNorm1d(2048, momentum=0.5)),
+            ('relu5', nn.ReLU()),
+            ('mpool5', nn.MaxPool1d(kernel_size=(3), stride=(2))),
+            ('fc6', nn.Conv1d(in_channels=2048, out_channels=4096, kernel_size=(1), stride=(1,1))),
+            ('bn6', nn.BatchNorm1d(4096, momentum=0.5)),
+            ('relu6', nn.ReLU()),
+            ('apool6', nn.AdaptiveAvgPool1d((1))),
             ('flatten', nn.Flatten())]))
             
         self.classifier=nn.Sequential(OrderedDict([
@@ -63,26 +49,9 @@ class AudioNet(nn.Module):
             ('fc8', nn.Linear(1024, n_classes))]))
     
     def forward(self, inp):
-        inp=self.block1(inp)
-        feat1 = self.extract1(inp).relu().mean(-1)
-        feat1 = torch.flatten(feat1, start_dim=1)
-        
-        inp=self.block2(inp)
-        feat2 = self.extract2(inp).relu().mean(-1)
-        feat2 = torch.flatten(feat2, start_dim=1)
-
-        
-        inp=self.block3(inp)
-        feat3 = self.extract3(inp).relu().mean(-1)
-        feat3 = torch.flatten(feat3, start_dim=1)
-        
-        
-        inp=self.block4(inp)
-        inp=self.block5(inp)
-        
-        inp = torch.cat([inp, feat1, feat2, feat3], 1)
-        print(inp.shape)
-        
+        inp = inp.squeeze()
+        inp=self.features(inp)
+        #inp=inp.view(inp.size()[0],-1)
         inp=self.classifier(inp)
         return inp
 
